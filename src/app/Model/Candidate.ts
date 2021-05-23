37ffe9223b46fcf8aa7ed0ab4560bc074333b9f1
aiswarya27 <aiswaryarajendran@gmail.com>
2021-05-23 15:56:01 +0530

    Added models

---
+++ src/app/Model/Candidate.ts
@@ -0,0 +1,8 @@
+export class Candidate {
+       
+    constructor(
+        public id:number,
+        public name:string,
+        public skillTags:string[]
+    ){}
+}
---
+++ src/app/Model/Job.ts
@@ -0,0 +1,9 @@
+export class Job {
+       
+    constructor(
+        public id:number,
+        public name:string,
+        public company:string,
+        public skillTags:string[]
+    ){}
+}
