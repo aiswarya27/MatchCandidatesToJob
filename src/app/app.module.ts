52cf29846017604497aa1a3b7c8ea87a83353ce7
aiswarya27 <aiswaryarajendran@gmail.com>
2021-05-23 16:31:24 +0530

    ais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/Common/http';
 import { AppComponent } from './app.component';
+import { CandidateListComponent } from './candidate-list/candidate-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandidateListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
